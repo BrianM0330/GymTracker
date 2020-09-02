43ec5278fd4102ca701ab5e231908eaf77b293ab
BrianM0330 <BrianM0330@gmail.com>
2020-09-02 11:44:31 -0500

    string table to integers

--- src/entity/popData.ts
+++ src/entity/popData.ts
@@ -3,24 +3,24 @@ import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
 @Entity()
 export class popData {
   @PrimaryGeneratedColumn()
-    date!: Date;
+    Date!: Date;
 
-  @Column()
-    time_8!: string;
-  @Column()
-    time_9!: string;
-  @Column()
-    time_10!: string;
-  @Column()
-    time_11!: string;
-  @Column()
-    time_12!: string;
-  @Column()
-    time_13!: string;
-  @Column()
-    time_14!: string;
-  @Column()
-    time_15!: string;
-  @Column()
-    time_16!: string;
+  @Column('int')
+    time_8!: Number;
+  @Column('int')
+    time_9!: Number;
+  @Column('int')
+    time_10!: Number;
+  @Column('int')
+    time_11!: Number;
+  @Column('int')
+    time_12!: Number;
+  @Column('int')
+    time_13!: Number;
+  @Column('int')
+    time_14!: Number;
+  @Column('int')
+    time_15!: Number;
+  @Column('int')
+    time_16!: Number;
 }
