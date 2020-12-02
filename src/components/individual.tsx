8516ce55e026b0337e570acffb26ea72fc5f6407
BrianM0330 <BrianM0330@gmail.com>
2020-12-02 12:28:53 -0600

    added business hours and fetch time

--- src/components/individual.tsx
+++ src/components/individual.tsx
@@ -3,19 +3,25 @@ import {useState, useEffect} from "react"
 import axios from 'axios'
 
 interface Props {
-    lastFetch: string,    
     locationName: string,
     locationURL: string,
 }
 
 export const Gym: React.FC<Props> = (props) => {
     const [count, setCount] = useState<number>()
+    const [fetchTime, setFetchTime] = useState<string>()
+    const [percentCapacity, setPercentCapacity] = useState<number>()
+    const [hoursOpen, setHoursOpen] = useState<string>()
     
     useEffect(() => {
         console.log('Fetching...')
+        var d = new Date()
+        setFetchTime(d.toLocaleTimeString('en-US'))
         const asyncFetch = async () => {
             const apiFetch = await axios.get(props.locationURL)
             setCount(apiFetch.data.occupancy.current)
+            setPercentCapacity(apiFetch.data.occupancy.percentage)
+            setHoursOpen(apiFetch.data.hours.description)
         }
         asyncFetch()
         // setCount(result.data)
@@ -23,8 +29,13 @@ export const Gym: React.FC<Props> = (props) => {
 
     return (
         <div>
-            <h1> {props.locationName} </h1>
-            <div> Amount as of {props.lastFetch}: {count}</div>
+            <h2>{props.locationName} - {percentCapacity}%</h2>
+            <div># of people: {count}</div>
+            <div>Last checked: {fetchTime}</div>
+            <br/>
+            <div> 
+                Current Hours<br/>{hoursOpen}
+            </div>
         </div>
     )
 }
