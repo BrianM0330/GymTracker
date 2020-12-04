b5f77c325cd3791e6cc7857cd6a7eb2f2acc5926
BrianM0330 <BrianM0330@gmail.com>
2020-12-04 16:39:19 -0600

    added distance calculator endpoint

--- src/index.ts
+++ src/index.ts
@@ -11,9 +11,12 @@ import { popDataResolver } from "./resolvers/popDataResolver"
 (async () => {
     const app = express();
     app.use(cors())
+    app.use(express.json())
 
     app.get('/:locationName', async function(_req, res) {
-      const urlToUse = urlsJson[_req.params.locationName.toLowerCase()]
+      const urlToUse = urlsJson[_req.params.locationName.toLowerCase()].url
+                        //locationName.url
+
       if (urlToUse == undefined) {
         console.log("Invalid location", _req.params.locationName.toLocaleUpperCase(), "entered")
         res.send('This location does not exist')
@@ -26,7 +29,53 @@ import { popDataResolver } from "./resolvers/popDataResolver"
       }
     })
 
+    app.post('/findNearest', function (_req, res) {
+      let closestLocations = {}
+      let coordinates = _req.body
+      let startingLat = coordinates.latitude
+      let startingLong = coordinates.longitude
+
+      for (let locObject in urlsJson) {
+        let latitude = urlsJson[locObject].latitude
+        let longitude = urlsJson[locObject].longitude
+
+        let miles = haversineLocationFormula(startingLat, startingLong, latitude, longitude)
+        
+        if (miles <= 5) { //can probably make this parameterized for user defined radius
+          closestLocations[locObject] = urlsJson[locObject]
+        }
+      }
+
+      if (Object.keys(closestLocations).length == 0) { //If there are no locations nearby..
+        console.log("None found")
+        return res.status(200).send({
+          success: false,
+          msg: "No locatitons found"
+        })
+      }
+
+      else {
+        console.log("Final locations", closestLocations)
+        return res.status(200).send(closestLocations)
+      }
+      
+    })
+
     await createConnection();
+
+    const haversineLocationFormula = (lat1: number, lon1: number, lat2: number, lon2: number) => {
+      var radiansLat1 = Math.PI * lat1/180
+      var radiansLat2 = Math.PI * lat2/180
+      var theta = lon1-lon2
+      var radiansTheta = Math.PI * theta/180
+      var dist = Math.sin(radiansLat1) * Math.sin(radiansLat2) + Math.cos(radiansLat1) * Math.cos(radiansLat2) * Math.cos(radiansTheta)
+      if (dist > 1)
+        dist = 1;
+      dist = Math.acos(dist)
+      dist = dist * 180/Math.PI
+      dist = dist*60*1.515
+      return dist
+    }
 
     //initializing apolloserver 
     const apolloServer = new ApolloServer({
