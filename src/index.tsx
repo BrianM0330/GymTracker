a957f9f4e13bdaa4c316b99bf9568220dda4324c
Brian <BrianM0330@gmail.com>
2020-12-16 13:22:41 -0600

    added react-bs imports

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Gym} from './components/individual'
+import {Gym} from './components/locationCard'
+import './index.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 ReactDOM.render(
-  <Gym 
-    lastFetch={"Recently"} 
-    locationName={"Cicero"} 
-    locationURL={"http://localhost:4000/cicero"}
-  />,
+  <div>
+    <Gym 
+      // lastFetch={"Recently"} 
+      locationName={"Cicero"} 
+      locationURL={"http://localhost:4000/cicero"}
+    />,
+    <Gym 
+    // lastFetch={"Recently"} 
+    locationName={"Forest Park"} 
+    locationURL={"http://localhost:4000/forest-park"}
+    />
+</div>, 
   document.getElementById('root')
 );
