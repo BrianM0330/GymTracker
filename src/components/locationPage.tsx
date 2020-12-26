01df3fab68ce3fafce9b15da2e739e1217f7dcf5
Brian <BrianM0330@gmail.com>
2020-12-25 18:09:23 -0600

    working gql query on useEffect hook

--- src/components/locationPage.tsx
+++ src/components/locationPage.tsx
@@ -1,42 +1,53 @@
-import * as React from 'react';
-import {ApolloClient, InMemoryCache, gql} from '@apollo/client'
-
-import axios from  'axios'
+import React, {useEffect, useState} from 'react';
+import {ApolloClient, InMemoryCache, gql, useQuery} from '@apollo/client'
+import {RouteComponentProps} from 'react-router'
+import {match} from "react-router-dom"
 import '../index.css'
 
-interface Props {
+interface Props extends RouteComponentProps {
     locationName: string
 }
 
-export const DetailedPage: React.FC<Props> = (props) => {
+export const LocationPage: React.FC<Props> = (props) => {
+    const [data, setData] = useState({})
 
-    const client = new ApolloClient({
-        uri: 'http://localhost:4000/graphql',
-        cache: new InMemoryCache()
-    })
+    useEffect(() => {
+        console.log(props.match.params['name'])
 
-    const populationQuery = gql
-    `
-        query {
-            locationHistory(location: ${props.locationName})
-            {
-                population_8
-                population_9
-                population_10
-                population_11
-                population_12
-                population_13
-                population_14
-                population_15
-                population_16
-            }
-        } 
-    `
-
-    client.query({
-        query: populationQuery
-    })
-    return <div></div>
+        const client = new ApolloClient({
+            uri: 'http://localhost:4000/graphql',
+            cache: new InMemoryCache()
+        })
+    
+        const GET_DATA = gql
+        `
+            query {
+                locationHistory(location: "${props.match.params['name']}")
+                {
+                    population_8
+                    population_9
+                    population_10
+                    population_11
+                    population_12
+                    population_13
+                    population_14
+                    population_15
+                    population_16
+                }
+            } 
+        `
+        client
+            .query({query: GET_DATA})
+            .then(result => setData(result.data.locationHistory))
+        
+    }, [setData])
+    
+    if ((Object).keys(data).length > 0) {
+        console.log(data)
+        return <div>Placeholder</div>
+    }
+    else
+        return <div>Fetching...</div> 
 }
 
 //Add graphs to this page
