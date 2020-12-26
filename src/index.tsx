6b3b58b39f2a6b68706fbbd2f6eb79b88e25a045
Brian <BrianM0330@gmail.com>
2020-12-25 18:10:20 -0600

    added react-router functionality

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,26 @@ import ReactDOM from 'react-dom';
 import {DeckOfLocations} from './components/deck'
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import { CardDeck } from 'react-bootstrap';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { LocationPage } from './components/locationPage';
+
+class App extends React.Component {
+  render () {
+    return (
+      <Router>
+        <div>
+          <Switch>
+            <Route exact path="/" component={DeckOfLocations}/>
+            <Route path="/location/:name" component={LocationPage}/>
+          </Switch>
+        </div>
+      </Router>
+    )
+  }
+}
 
 ReactDOM.render(
   //Deck of cards with location data from card components gets rendered.
-  <DeckOfLocations />, document.getElementById('root')
+  <App />, 
+  document.getElementById('root')
 );
