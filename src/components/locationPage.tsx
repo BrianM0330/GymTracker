f717a5b9ebb9f37eaaf5d3f0f2c5049b0adbd1cd
Brian <BrianM0330@gmail.com>
2020-12-30 15:45:53 -0600

    single location graphs added

--- src/components/locationPage.tsx
+++ src/components/locationPage.tsx
@@ -1,9 +1,11 @@
 import React, {useEffect, useState} from 'react';
-import {ApolloClient, InMemoryCache, gql, useQuery} from '@apollo/client'
+import {ApolloClient, InMemoryCache, gql} from '@apollo/client'
 import {RouteComponentProps} from 'react-router'
-import {match} from "react-router-dom"
+import {ColumnChart} from "@toast-ui/react-chart"
+import 'tui-chart/dist/tui-chart.css'
 import '../index.css'
 
+//TODO: For comparing all locations, pass the data in props and add it to graphs in series
 interface Props extends RouteComponentProps {
     locationName: string
 }
@@ -55,16 +57,80 @@ export const LocationPage: React.FC<Props> = (props) => {
             .query({query: GET_DATA})
             .then(result => setData(result.data.locationHistory))
         
-    }, [setData])
-    
+    }, [setData, props.match.params])
+
     if ((Object).keys(data).length > 0) {
-        console.log(data)
-        console.log(data['population_14'])
-        return <div>Placeholder</div>
+        //https://nhn.github.io/tui.chart/latest/tui.chart
+        //Docs for chart library
+
+        const formattedLocationName = props.match.params['name']
+                                    .replace('-', ' ')
+                                    .toLowerCase()
+                                    .split(' ')
+                                    .map((s) => s.charAt(0).toUpperCase() + s.substring(1))
+                                    .join(' ')
+        
+        //Object.values was returning type: unknown[]
+        const typedData: number[] = Object.values(data)
+        const labels: string[] = []
+        
+        //Removes population_ substring from the GQL query
+        Object.keys(data).map(x => {
+            if (x !== '__typename') {
+                var time: number = eval(x.substr(11))
+                var timeFormat = 
+                (time >= 12 && time<=23)  //Times 12PM - 11PM
+                ? "PM"
+                : "AM"
+
+                if (time > 12) labels.push(`${time-12} ${timeFormat}`)
+                else if (time === 12) labels.push(`${time} ${timeFormat}`)
+                else if (time === 0) labels.push(`${time+12} ${timeFormat}`)
+                else labels.push(`${time} ${timeFormat}`)
+            }
+        })
+
+        var formattedData = {
+            categories: labels,
+
+            //To add other lines, just create another series with more data + name.
+            series: [
+                {
+                    name: `${formattedLocationName}`,
+                    data: typedData
+                },
+            ]
+        }
+
+        var options = {
+            chart: {
+                width: window.innerWidth,
+                height: window.innerHeight,
+                title: `${formattedLocationName} Population History`,
+                // format: '5'
+            },
+            yAxis: {
+                title: 'Population count',
+                min: 0,
+                max: 60
+            },
+            xAxis: {
+                title: 'Times',
+                pointOnColumn: true
+            }
+        }
+
+        return (
+            <div>
+                <ColumnChart 
+                    data={formattedData}
+                    options={options}
+                />
+            </div>
+        )
     }
-    else
+    else //Loading page
         return <div>Fetching...</div> 
 }
 
-//Add graphs to this page
 //Stylize everything
