93113740dd498c321e1954bcef741eeb22ef687e
Brian <BrianM0330@gmail.com>
2021-01-01 18:18:03 -0600

    multiple locations now queried and graphed

--- src/components/locationPage.tsx
+++ src/components/locationPage.tsx
@@ -1,122 +1,136 @@
 import React, {useEffect, useState} from 'react';
 import {ApolloClient, InMemoryCache, gql} from '@apollo/client'
-import {RouteComponentProps} from 'react-router'
+import {RouteComponentProps, StaticContext} from 'react-router'
 import {ColumnChart} from "@toast-ui/react-chart"
 import 'tui-chart/dist/tui-chart.css'
 import '../index.css'
 
-//TODO: For comparing all locations, pass the data in props and add it to graphs in series
-interface Props extends RouteComponentProps {
-    locationName: string
-}
+type Props = RouteComponentProps<{}, StaticContext, {from: {pathname: String}}>
 
 export const LocationPage: React.FC<Props> = (props) => {
-    const [data, setData] = useState({})
+    const [data, setData] = useState([{}])
 
     useEffect(() => {
-        console.log(props.match.params['name'])
 
         const client = new ApolloClient({
             uri: 'http://localhost:4000/graphql',
-            cache: new InMemoryCache()
+            cache: new InMemoryCache({
+                addTypename: false //Removes '__typename' key from the query result
+            })
         })
-    
-        const GET_DATA = gql
-        `
-            query {
-                locationHistory(location: "${props.match.params['name']}")
-                {
-                    population_0
-                    population_1
-                    population_2
-                    population_3
-                    population_4
-                    population_5
-                    population_6
-                    population_7
-                    population_8
-                    population_9
-                    population_10
-                    population_11
-                    population_12
-                    population_13
-                    population_14
-                    population_15
-                    population_16
-                    population_17
-                    population_18
-                    population_19
-                    population_20
-                    population_21
-                    population_22
-                    population_23
-                }
-            } 
-        `
-        client
-            .query({query: GET_DATA})
-            .then(result => setData(result.data.locationHistory))
+
+        for (let location in props.location.state) {
+
+            const GET_DATA = gql
+            `
+                query {
+                    locationHistory(location: "${location}")
+                    {
+                        population_0
+                        population_1
+                        population_2
+                        population_3
+                        population_4
+                        population_5
+                        population_6
+                        population_7
+                        population_8
+                        population_9
+                        population_10
+                        population_11
+                        population_12
+                        population_13
+                        population_14
+                        population_15
+                        population_16
+                        population_17
+                        population_18
+                        population_19
+                        population_20
+                        population_21
+                        population_22
+                        population_23
+                    }
+                } 
+            `
+            client
+                .query({query: GET_DATA})
+                //...notation will just ADD a new object to the existing array
+                .then(result => setData(prevState => [...prevState, {[location]: result.data.locationHistory}]))
+        }
         
-    }, [setData, props.match.params])
+    }, [props.match.params, setData])
 
-    if ((Object).keys(data).length > 0) {
-        //https://nhn.github.io/tui.chart/latest/tui.chart
-        //Docs for chart library
+    if (data.length === (Object).keys(props.location.state).length+1) {
 
-        const formattedLocationName = props.match.params['name']
+        const primaryLocationName = props.match.params['name']
                                     .replace('-', ' ')
                                     .toLowerCase()
                                     .split(' ')
                                     .map((s) => s.charAt(0).toUpperCase() + s.substring(1))
                                     .join(' ')
-        
-        //Object.values was returning type: unknown[]
-        const typedData: number[] = Object.values(data)
-        const labels: string[] = []
-        
-        //Removes population_ substring from the GQL query
-        Object.keys(data).map(x => {
-            if (x !== '__typename') {
-                var time: number = eval(x.substr(11))
-                var timeFormat = 
-                (time >= 12 && time<=23)  //Times 12PM - 11PM
-                ? "PM"
-                : "AM"
 
-                if (time > 12) labels.push(`${time-12} ${timeFormat}`)
-                else if (time === 12) labels.push(`${time} ${timeFormat}`)
-                else if (time === 0) labels.push(`${time+12} ${timeFormat}`)
-                else labels.push(`${time} ${timeFormat}`)
+        let seriesArray: [{
+                name: string,
+                data: number[]
+            }
+        ] =  [{name: "", data: []}]
+        seriesArray.pop() //Removes the empty value from initialization
+
+        data.map(x => {
+            const nameString = Object.keys(x)[0]
+            if (nameString) { //Removes the empty {} from initialization in useState
+                //@ts-ignore
+                const theData: number[] = Object.values(x)[0]
+
+                const toPush = {
+                    name: nameString,
+                    data: Object.values(theData)
+                }
+                seriesArray.push(toPush)
             }
         })
 
+
+        /*
+        https://nhn.github.io/tui.chart/latest/tui.chart
+        Docs for chart library 
+        */
+       
+        const labels: string[] = [
+        "12 AM", "1 AM", "2 AM", "3 AM",
+        "4 AM", "5 AM", "6 AM", "7 AM", 
+        "8 AM", "9 AM", "10 AM", "11 AM", 
+        "12 PM", "1 PM", "2 PM", "3 PM", 
+        "4 PM", "5 PM", "6 PM", "7 PM", 
+        "8 PM", "9 PM", "10 PM", "11 PM", 
+        ]
+
         var formattedData = {
             categories: labels,
-
-            //To add other lines, just create another series with more data + name.
-            series: [
-                {
-                    name: `${formattedLocationName}`,
-                    data: typedData
-                },
-            ]
+            series: seriesArray
         }
 
         var options = {
             chart: {
                 width: window.innerWidth,
                 height: window.innerHeight,
-                title: `${formattedLocationName} Population History`,
-                // format: '5'
+                title: {
+                    text: `${primaryLocationName} Population History`,
+                    align: "center"
+                }
             },
             yAxis: {
                 title: 'Population count',
                 min: 0,
-                max: 60
+                max: 50
             },
             xAxis: {
                 title: 'Times',
                 pointOnColumn: true
+            },
+            legend: {
+                align: "bottom"
             }
         }
 
@@ -129,7 +143,7 @@ export const LocationPage: React.FC<Props> = (props) => {
             </div>
         )
     }
-    else //Loading page
+    else
         return <div>Fetching...</div> 
 }
 
